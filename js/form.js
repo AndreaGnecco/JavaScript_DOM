50d2e0381fa933b837f794baafd3b7ca40ccf9a0
andreagnecco <andreagnecco@gmail.com>
2018-01-16 21:54:20 -0200

    objeto endereco e contato

--- js/form.js
+++ js/form.js
@@ -1,27 +1,39 @@
 var dadosCliente = {
     endereco: {
         pais : '',
-        cidade : ''
+        cidade : '',
+        estado : '',
+        endereco1: '',
+        endereco2 : ''
     },
 
     contato : {
         nome : '',
-        email : ''
+        sobrenome : '',
+        email : '',
+        fone : ''
         }
 };
 
 function validarFormulario(){
     //gerar objeto endereco baseado nos dados do validarFormulario
     objEndereco = {
-        pais : document.getElementById('selection_country').value
+        pais : document.getElementById('selection_country').value,
+        cidade : document.getElementById('selection_city').value,
+        estado : document.getElementById('state').value,
+        endereco1 : document.getElementById('ad1').value,
+        endereco2 : document.getElementById('ad2').value
     };
 
     dadosCliente.endereco = objEndereco;
     console.log(objEndereco);
-    
+
     //gerar objeto contato
     objContato = {
-        nome : document.getElementById('name').value
+        nome : document.getElementById('name').value,
+        sobrenome : document.getElementById('lastname').value,
+        email : document.getElementById('email').value,
+        fone : document.getElementById('phone').value
     };
 
     dadosCliente.contato = objContato;
