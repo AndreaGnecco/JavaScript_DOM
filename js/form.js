4d5bdc69efa0de59c16b5a7a4fe78881b6b2b221
andreagnecco <andreagnecco@gmail.com>
2018-01-17 21:13:18 -0200

    vbcvb

--- js/form.js
+++ js/form.js
@@ -1,44 +1,78 @@
 var dadosCliente = {
-    endereco: {
-        pais : '',
-        cidade : '',
-        estado : '',
-        endereco1: '',
-        endereco2 : ''
-    },
-
-    contato : {
-        nome : '',
-        sobrenome : '',
-        email : '',
-        fone : ''
-        }
+    endereco: {},
+    contato: {}
 };
 
-function validarFormulario(){
-    //gerar objeto endereco baseado nos dados do validarFormulario
+function validarFormulario() {
+
+    event.preventDefault();
+
+    if (validarEndereco() && validarContato())
+        console.log('validação está ok.. pode prosseguir');
+    else
+        console.log('validação não passou.. preencher todos os campos');
+};
+
+function validarEndereco() {
+    //gerar objeto endereco baseado nos dados do formulario
     objEndereco = {
-        pais : document.getElementById('selection_country').value,
-        cidade : document.getElementById('selection_city').value,
-        estado : document.getElementById('state').value,
-        endereco1 : document.getElementById('ad1').value,
-        endereco2 : document.getElementById('ad2').value
+        pais: document.getElementById('selection_country').value,
+        cidade: document.getElementById('selection_city').value,
+        estado: document.getElementById('state').value,
+        endereco1: document.getElementById('ad1').value,
+        endereco2: document.getElementById('ad2').value,
     };
 
-    dadosCliente.endereco = objEndereco;
-    console.log(objEndereco);
+    //verificar se os dados estão preenchidos
+    if (objEndereco.pais == '0') {
+        return false;
+    } else if (objEndereco.cidade.trim().length == '0') {
+        return false;
+    } else if (objEndereco.estado.trim().length == 0) {
+        return false;
+    } else if (objEndereco.endereco1.trim().length == 0) {
+        return false;
+    } else if (objEndereco.endereco2.trim().length == 0) {
+        return false;
+    } else {//validação ok
+        //grava o objeto endereço no objeto dadosCliente
+        dadosCliente.endereco = objEndereco;
+        //retorno da função
+        return true;
+    }
 
-    //gerar objeto contato
+
+function validarContato(_objContato) {
+     //gerar objeto contato baseado nos dados do formulario
     objContato = {
-        nome : document.getElementById('name').value,
-        sobrenome : document.getElementById('lastname').value,
-        email : document.getElementById('email').value,
-        fone : document.getElementById('phone').value
+        nome: document.getElementById('name').value,
+        sobrenome: document.getElementById('lastname').value,
+        email: document.getElementById('email').value,
+        fone: document.getElementById('phone').value,
     };
 
-    dadosCliente.contato = objContato;
-    console.log(objContato);
-}
+    if(objContato.nome == 0) {
+        return false;
+    } else if(objContato.sobrenome == 0) {
+        return false;
+    } else if(objContato.email == 0) {
+        return false;
+    } else {
+        return true;
+    }
 
-    
-    
+
+     if(document.getElementById('s_input').checked ){
+         var pacote1 = document.getElementById('s_input').value;
+     } else if(document.getElementByI('u_input').checked){
+        var pacote2 = document.getElementById('u_input');
+     } else {
+         return false;
+     }
+
+     objContato = {
+        pacote : pacote,
+};
+
+         console.log( pacote )
+}
